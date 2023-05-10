fb590b2ff2bb7f5c4fbc55bd550ddc7998df2f1a
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2023-05-10 14:50:46 +0900

    commit

--- store-app/Factory-app/Factory/src/test/java/factoryapp/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package factoryapp.common;
-
-import factoryapp.FactoryApplication;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { FactoryApplication.class })
-public class CucumberSpingConfiguration {}
